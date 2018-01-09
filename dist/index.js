90977b896c1a617a32d58866db4c674f69240bf7
GABRIEL DE SOUZA SILVA <GABRIEL.SI@uninter.com>
2018-01-09 11:17:33 -0200

    added import in index.js

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+import { Component, Input } from '@angular/core';
 "use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
