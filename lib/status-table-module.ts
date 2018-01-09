416249a61b60f0cbf68c784c1291ec17bbed72bb
GABRIEL DE SOUZA SILVA <GABRIEL.SI@uninter.com>
2018-01-09 14:53:05 -0200

    created statusTable module

---
+++ lib/status-table-module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { StatusTableComponent } from "./status-table.component";
+
+
+@NgModule({
+    imports: [
+
+    ], exports: [
+
+    ], declarations: [
+        StatusTableComponent
+    ],
+    providers: [
+    ]
+})
+export class StatusTableModule {
+}
+
+
+
+
