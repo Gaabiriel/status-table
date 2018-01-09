83ca2a7a0788714db73b8d120a4a7710aa56fc79
GABRIEL DE SOUZA SILVA <GABRIEL.SI@uninter.com>
2018-01-09 15:45:43 -0200

    update

---
+++ dist/index.d.ts
@@ -0,0 +1,10 @@
+import { OnInit } from '@angular/core';
+import { ViewCell } from 'ng2-smart-table';
+export declare class StatusTableComponent implements ViewCell, OnInit {
+    renderValue: string;
+    directiveValue: boolean;
+    value: string | number;
+    rowData: any;
+    statusBadge: string;
+    ngOnInit(): void;
+}
--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,30 @@
-import * from './status-table-module';
+import { Component, Input, OnInit, Directive, Renderer, ElementRef } from '@angular/core';
+import { ViewCell } from 'ng2-smart-table';
+
+
+@Component({
+  selector: 'status-table',
+  styleUrls: [
+    
+  ],
+  template: `
+    <div class="m-badge {{statusBadge}}">{{renderValue}}</div>
+  `,
+})
+
+export class StatusTableComponent implements ViewCell, OnInit {
+
+  renderValue: string;
+  directiveValue: boolean;
+  @Input() value: string | number;
+  @Input() rowData: any;
+  statusBadge: string;
+  ngOnInit() {
+    this.renderValue = this.value ? 'Ativo' : 'Inativo';
+    if (this.value) {
+      this.statusBadge = 'm-badge--success';
+    } else {
+      this.statusBadge = 'm-badge--danger';
+    }
+  }
+}
